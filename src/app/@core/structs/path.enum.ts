66a390e10e646c7cfe888efb1c3c464dd802943d
stevenclar <deividclar@gmail.com>
2021-06-06 19:44:59 -0500

    Add Guards, auth service and path

---
+++ src/app/@core/structs/path.enum.ts
@@ -0,0 +1,17 @@
+export enum Path {
+  // General containers
+  Home = '',
+  NotFound = '404',
+
+  // Auth
+  SignIn = 'sign-in',
+  SignUp = 'sign-up',
+
+  // App base url
+  Dashboard = 'dashboard',
+
+  // User
+  Users = 'users',
+  UsersOverview = 'overview',
+  UsersProfile = ':username',
+}
