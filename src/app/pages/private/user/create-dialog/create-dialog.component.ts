5c398905457f2634a6191e38adf65bf425e1fc55
stevenclar <deividclar@gmail.com>
2021-06-08 09:05:42 -0500

    Typo and limit fields

--- src/app/pages/private/user/create-dialog/create-dialog.component.ts
+++ src/app/pages/private/user/create-dialog/create-dialog.component.ts
@@ -33,11 +33,11 @@ export class CreateDialogComponent implements OnInit {
 
   ngOnInit(): void {
     this.newUserFormGroup = this._formBuilder.group({
-      name: [this.data?.name ?? '', [Validators.compose([Validators.required, Validators.pattern('[A-ZÑa-zñÀ-ÿ]+[A-ZÑa-zñÀ-ÿ ]*'), Validators.minLength(3), Validators.maxLength(45)])]],
-      username: [this.data?.username ?? '', [Validators.compose([Validators.required, Validators.pattern('[A-ZÑa-zñÀ-ÿ]+[A-ZÑa-zñÀ-ÿ ]*'), Validators.minLength(3), Validators.maxLength(45)])]],
-      email: [this.data?.email ?? '', [Validators.compose([Validators.required, Validators.pattern('[ÑA-Zña-z0-9._%+-]{3,}@[a-zA-Z]{3,}([.]{1}[a-zA-Z]{2,}|[.]{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,})'), Validators.minLength(4), Validators.maxLength(70)])]],
+      name: [this.data?.name ?? '', [Validators.compose([Validators.required, Validators.pattern('[A-ZÑa-zñÀ-ÿ]+[A-ZÑa-zñÀ-ÿ ]*'), Validators.minLength(3), Validators.maxLength(15)])]],
+      username: [this.data?.username ?? '', [Validators.compose([Validators.required, Validators.pattern('[A-ZÑa-zñÀ-ÿ]+[A-ZÑa-zñÀ-ÿ ]*'), Validators.minLength(3), Validators.maxLength(15)])]],
+      email: [this.data?.email ?? '', [Validators.compose([Validators.required, Validators.pattern('[ÑA-Zña-z0-9._%+-]{3,}@[a-zA-Z]{3,}([.]{1}[a-zA-Z]{2,}|[.]{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,})'), Validators.minLength(4), Validators.maxLength(30)])]],
       phone: [this.data?.phone ?? '', [Validators.compose([Validators.required, Validators.pattern('^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$'), Validators.minLength(3), Validators.maxLength(30)])]],
-      website: [this.data?.website ?? '', [Validators.compose([Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')])]],
+      website: [this.data?.website ?? '', [Validators.compose([Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?'), Validators.minLength(4), Validators.maxLength(30)])]],
     });
     this.getErrorMessage()
   }
@@ -48,19 +48,19 @@ export class CreateDialogComponent implements OnInit {
         { type: 'required', message: 'El nombre es requerido' },
         { type: 'pattern', message: 'Se debe ingresar un nombre válido' },
         { type: 'minlength', message: 'Mínimo 3 caracteres' },
-        { type: 'maxlength', message: 'Máximo 45 caracteres' },
+        { type: 'maxlength', message: 'Máximo 15 caracteres' },
       ],
       'username': [
         { type: 'required', message: 'El usuario es requerido' },
         { type: 'pattern', message: 'El usuario debe ser alfanumérico' },
         { type: 'minlength', message: 'Mínimo 3 caracteres' },
-        { type: 'maxlength', message: 'Máximo 45 caracteres' },
+        { type: 'maxlength', message: 'Máximo 15 caracteres' },
       ],
       'email': [
         { type: 'required', message: 'El correo electrónico es requerido' },
-        { type: 'pattern', message: 'Se debe ingrsar un correo electrónico válido' },
+        { type: 'pattern', message: 'Se debe ingresar un correo electrónico válido' },
         { type: 'minlength', message: 'Mínimo 4 caracteres' },
-        { type: 'maxlength', message: 'Máximo 70 caracteres' },
+        { type: 'maxlength', message: 'Máximo 30 caracteres' },
       ],
       'phone': [
         { type: 'required', message: 'El teléfono es requerido' },
@@ -72,7 +72,7 @@ export class CreateDialogComponent implements OnInit {
         { type: 'required', message: 'El sitio web es requerido' },
         { type: 'pattern', message: 'El sitio web debe ser una url' },
         { type: 'minlength', message: 'Mínimo 4 caracteres' },
-        { type: 'maxlength', message: 'Máximo 70 caracteres' },
+        { type: 'maxlength', message: 'Máximo 30 caracteres' },
       ],
     };
   }
--- src/app/pages/private/user/user.component.ts
+++ src/app/pages/private/user/user.component.ts
@@ -57,6 +57,7 @@ export class UserComponent implements OnInit, OnDestroy {
     dialogRef.afterClosed().subscribe(newUser => {
       newUser && this.userService.createUser(newUser)
         .subscribe((createdUser) => {
+          createdUser.id = 100
           this.users = [createdUser, ...this.users].slice(0, 5)
         })
     });
